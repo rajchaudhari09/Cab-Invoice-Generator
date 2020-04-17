2b67a2557bea8d63a9657a22b93e43db0fbc1090
rajchaudhari09 <rajchaudhari366@gmail.com>
2020-04-17 18:53:54 +0530

    ADDED: Calculation for code to multiple ride

--- src/test/java/CabInvoiceGeneratorTest.java
+++ src/test/java/CabInvoiceGeneratorTest.java
@@ -1,7 +1,7 @@
 import org.junit.Assert;
 import org.junit.Test;
 
-public class CabInvoiceGenerator {
+public class CabInvoiceGeneratorTest {
     InvoiceGenerator invoiceGenerator=new InvoiceGenerator();
 
     @Test
@@ -18,5 +18,15 @@ public class CabInvoiceGenerator {
         int time=1;
         double fare=invoiceGenerator.calculateFare(distance,time);
         Assert.assertEquals(5,fare,0.0);
+    }
+
+    @Test
+    public void givenMultipleRides_ShouldReturnTotalFare() {
+        Ride[] rides = {
+                new Ride(2.0, 5),
+                new Ride(0.1, 1)
+        };
+        double fare = invoiceGenerator.calculateFare(rides);
+        Assert.assertEquals(30 ,fare,0.0);
     }
 }
