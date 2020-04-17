d9563de07f0fa8ca09853ccdf16284e5f9146862
rajchaudhari09 <rajchaudhari366@gmail.com>
2020-04-17 18:49:29 +0530

    ADDED: Given distance and time, the invoice generator should return the total fare for the journey

--- src/test/java/CabInvoiceGenerator.java
+++ src/test/java/CabInvoiceGenerator.java
@@ -12,4 +12,11 @@ public class CabInvoiceGenerator {
         Assert.assertEquals(25,fare,0.0);
     }
 
+    @Test
+    public void givenDistanceAndTime_ShouldReturnMinimumFare(){
+        double distance= 0.1;
+        int time=1;
+        double fare=invoiceGenerator.calculateFare(distance,time);
+        Assert.assertEquals(5,fare,0.0);
+    }
 }
